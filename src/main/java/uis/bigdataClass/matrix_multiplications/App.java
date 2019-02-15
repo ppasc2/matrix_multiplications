11aca571ba0935eb0c803e81fa0e217d7882d631
Peevee Pascual <@ppasc2@uis.edu>
2019-02-15 12:44:35 -0700

    Initial

---
+++ src/main/java/uis/bigdataClass/matrix_multiplications/App.java
@@ -0,0 +1,13 @@
+package uis.bigdataClass.matrix_multiplications;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
